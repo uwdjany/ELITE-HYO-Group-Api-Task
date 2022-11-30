7dbaa18cf84ce0bfe5a1c51dc36b6295c7552881
uwdjany <djanatiuwase@gmail.com>
2022-11-30 23:06:31 +0200

    model and services added

--- src/server.js
+++ src/server.js
@@ -1,10 +1,18 @@
 import  express  from "express";
 import "dotenv/config";
 import bodyParser from "body-parser";
+import mongoose from "mongoose";
+
 
 const app = express();
 app.use(bodyParser.json());
+// app.use("/v1/acronym",routes)
 
+const dbUrl = process.env.DATABASE_Url;
+mongoose.connect(dbUrl,{useNewUrlParser:true,useUnifiedTopology:true,})
+.then(()=>{
+    console.log("Database Connected Successfuly");
+})
 const port = process.env.PORT || 4045;
 app.listen(port, () =>{
     console.log(`server is running on port ${port}`)
